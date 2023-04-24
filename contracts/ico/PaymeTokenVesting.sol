c4119964baf5b9198523f6c26101deca12f0d9e8
PayMe-Currency <currency@paymeplatforms.com>
2023-04-25 00:06:45 +0100

    added MIT licence

--- contracts/ico/PaymeTokenVesting.sol
+++ contracts/ico/PaymeTokenVesting.sol
@@ -1,5 +1,4 @@
-// ico/contracts/PaymeTokenVesting.sol
-// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
